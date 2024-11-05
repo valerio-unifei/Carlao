6fc3850fa49414059d085d0667199d498388de74
valerio-unifei <valerio@unifei.edu.br>
2024-11-05 09:02:03 -0300

    Add control handler for button action and update setup for access point

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,29 @@ void handleRoot() {
   Serial.println("Pagina Enviada ao Cliente");
 }
 
+void handleControl(){
+  if (server.hasArg("action")) {
+    String action = server.arg("action");
+    if (action == "acionar") {
+      Serial.println("Botão Acionar pressionado");
+      digitalWrite(26, !digitalRead(26));
+    }
+  }
+  server.send(200, "text/html", "<html><body><h1>Controle</h1><form action=\"/control\" method=\"POST\"><button type=\"submit\" name=\"action\" value=\"acionar\">Acionar</button></form></body></html>");
+}
+
 void setup() {
   Serial.begin(115200);
-  WiFi.softAP(ssid, password);
+  WiFi.softAP(ssid, password,10);
+
+  pinMode(26, OUTPUT);
 
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
 
   server.on("/", handleRoot);
+  server.on("/control", handleControl);
   server.begin();
   Serial.println("HTTP server started");
 }
